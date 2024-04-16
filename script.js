9a474b58739032e983ac44d5e094c08407698d84
Abhishek Kumar <144824988+Archie3105@users.noreply.github.com>
2024-04-16 12:52:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const socket = io('http://localhost:7500')
+
+
 const darkMode = document.getElementById("darkMode");
 const lightMode = document.getElementById("lightMode");
 const Mode = document.getElementById("mode");
@@ -45,8 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             scrollToTopBtn.style.display = "none";
         }
-
-        
     });
   
     scrollToTopBtn.addEventListener("click", function () {
@@ -63,8 +64,80 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 
+  // ********************Live Chat******************
+
+const name = document.getElementsByClassName('nameInput').value;
+const email = document.getElementsByClassName('message-input').value;
+const phone = document.getElementsByClassName('message-input').value;
+const startBtn = document.getElementsByClassName('startBtn');
+const form = document.getElementById('send-container');
+const  messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".liveChat");
+var audio = new Audio('./media/ting.mp3');
+
+
+//   const name = prompt("Enter your name to join");
+
+  const append = (message, position) => {
+      const messageElement = document.createElement('div');
+      messageElement.innerText = message;
+      messageElement.classList.add('message');
+      messageElement.classList.add(position);
+      messageContainer.append(messageElement);
+  
+      if(position == 'left' ){
+          audio.play();
+      }
+  }
 
 
 
+//   function nextPage(e) {
+//     e.preventDefault();
+// //     const name = document.getElementsByClassName('nameInput').value;
+//     const email = document.getElementsByClassName('message-input').value;
+//     const phone = document.getElementsByClassName('message-input').value;
 
+//     const liveChatContainer = document.getElementsByClassName('liveChatContainer');
+//     const chatbox = document.getElementById('chatbox');
+//     // Display the username on the next page
+//     // document.getElementById("output").textContent = name;
+
+//     // Hide the current page and display the next page
+//     chatbox.style.visibility = "hidden";
+//     liveChatContainer.style.visibility = "visible";
+
+// }
+const nextPage = (e) =>{
+  e.preventDefault();
+  chatbox.style.display = "none";
+  liveChatContainer.style.display = "block";
+
+}
+  
+  
+  form.addEventListener('submit',(e) => {
+      e.preventDefault();
+      const message = messageInput.value;
+      append(`You: ${message}`, 'right');
+      socket.emit('send',message);
+      messageInput.value="";
+  })
+  
+  
+  
+  socket.emit('new-user-joined', name);
+  
+  socket.on('user-joined', name => {
+      append(`${name} joined the chat`, 'right')
+  })
+  
+  socket.on('receive', data => {
+      append(`${data.name}: ${data.message}`, 'left')
+  })
+  
+  socket.on('left', name => {
+      append(`${data.name} left the chat`, 'left')
+  })
+    
 
